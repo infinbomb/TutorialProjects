c2df66715b6dc7e1fd51434ca953a589ed04fff4
infinbomb <99835573+infinbomb@users.noreply.github.com>
2022-05-26 13:38:47 -0400

    Update pathfinder.py

--- pathfinderpractice/pathfinder.py
+++ pathfinderpractice/pathfinder.py
@@ -17,11 +17,12 @@ maze = [
 
 
 def print_maze(maze, stdscr, path=[]):
+    # prints maze to screen using the color pairs
     BLUE = curses.color_pair(1)
     RED = curses.color_pair(2)
 
     for i, row in enumerate(maze):
-        for j, value in enumerate(row):
+        for j, value in enumerate(row): # like a for each loop
             if (i, j) in path:
                 stdscr.addstr(i, j*2, "X", RED)
             else:
@@ -29,10 +30,11 @@ def print_maze(maze, stdscr, path=[]):
 
 
 def find_start(maze, start):
+    # just finds the entrance to the maze "O"
     for i, row in enumerate(maze):
         for j, value in enumerate(row):
-            if value == start:
-                return i, j
+            if value == start: # uses parameter
+                return i, j # returns the 2d array index
 
     return None
 
@@ -42,30 +44,31 @@ def find_path(maze, stdscr):
     end = "X"
     start_pos = find_start(maze, start)
 
-    q = queue.Queue()
-    q.put((start_pos, [start_pos]))
+    q = queue.Queue() #Queue is a queue
+    q.put((start_pos, [start_pos])) #puts start position in the queue
 
     visited = set()
 
-    while not q.empty():
+    while not q.empty(): # returns true or false
         current_pos, path = q.get()
         row, col = current_pos
 
-        stdscr.clear()
+        stdscr.clear() # clears the screen
         print_maze(maze, stdscr, path)
-        time.sleep(0.2)
+        time.sleep(0.2) # wait .2 secs
         stdscr.refresh()
 
-        if maze[row][col] == end:
+        if maze[row][col] == end: # return the path when it reaches X, the end
             return path
 
         neighbors = find_neighbors(maze, row, col)
         for neighbor in neighbors:
             if neighbor in visited:
-                continue
+                # if just visited
+                continue # skip that iteration of loop
 
             r, c = neighbor
-            if maze[r][c] == "#":
+            if maze[r][c] == "#": # if wall, skip
                 continue
 
             new_path = path + [neighbor]
@@ -74,6 +77,7 @@ def find_path(maze, stdscr):
 
 
 def find_neighbors(maze, row, col):
+    # just finds and returns all neighbors thats in the maze, including border
     neighbors = []
 
     if row > 0:  # UP
@@ -89,11 +93,11 @@ def find_neighbors(maze, row, col):
 
 
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK) # number, foreground color, background color
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
     find_path(maze, stdscr)
-    stdscr.getch()
+    stdscr.getch()  # ?
 
 
-wrapper(main)
+wrapper(main) # importing the curses.wrapper() function and using it like this will make debugging easier
